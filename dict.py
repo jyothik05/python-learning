a0677f1907e58ac64c19ff4eb44db231aa413f67
jyothik05 <jyothikaswamykore@gmail.com>
2024-08-23 15:34:24 +0530

    Delete dict.py

--- dict.py
+++
@@ -1,9 +0,0 @@
-dict1 ={"Name":"lilly","Age":20,"gender":"Female"}
-dict1["Name"] = "Rosy"
-print(dict1)
-dict1["Branch"] = "ECE"
-print(dict1)
-print(dict1.keys())
-print(dict1.values())
-print(dict1.items())
-print(dict1[0])
